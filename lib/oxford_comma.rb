40340268da57f1afb244c30f63a3679a3c658c47
C-Lion Stepansky <esstepansky@gmail.com>
2021-05-16 11:49:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,27 @@
 def oxford_comma(array)
-  array = array
-  string1 = array[1].join(", ")
-  string.push(string1)
-  string2  = array[2].join(", ")
-  string.push(string2)
-  string.push("and ")
-  string3 = array[3]
-  string.push string3
+  case array.size 
+  when 1 
+    array
+  when 2 
+    array.split(" and ")
+  when 3 
+    string1 = array[1].join(", ")
+    string.push(string1)
+    string2  = array[2].join(", ")
+    string.push(string2)
+    string.push("and ")
+    string3 = array[3]
+    string.push string3
+    
+  else
+    final3 = 
+    - = array[-3].join(", ")
+    string.push(string1)
+    string2  = array[-2].join(", ")
+    string.push(string2)
+    string.push("and ")
+    string3 = array[-1]
+    string.push string3
+    
 
 end
