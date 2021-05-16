db45b5eadce1b7dd0123c495c023ae935f641446
C-Lion Stepansky <esstepansky@gmail.com>
2021-05-16 11:42:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,11 @@
 def oxford_comma(array)
-  array.join(", ")
+  array = array
+  string1 = array[1].join(", ")
+  string.push(string1)
+  string2  = array[2].join(", ")
+  string.push(string2)
+  string.push("and ")
+  string3 = array[3]
+  string.push string3
 
 end
